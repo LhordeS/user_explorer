7fefc984a75f053c2e6e4fe8c2da97f6d9d12b99
LhordeS <60381121+LhordeS@users.noreply.github.com>
2026-03-17 21:58:58 +0900

    Add HTML content, write script, and modify CSS styling

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 "use strict";
+
+const list = document.getElementById("users");
+
+fetch("https://randomuser.me/api/?results=5")
+  .then((res) => res.json())
+  .then((data) => {
+    data.results.forEach((user) => {
+      if (user.location.country === "Canada") {
+        const li = document.createElement("li");
+
+        const img = document.createElement("img");
+        img.src = user.picture.thumbnail;
+
+        const name = `${user.name.first} ${user.name.last}`;
+        const country = user.location.country;
+
+        li.appendChild(img);
+        li.appendChild(document.createTextNode(`${name} from ${country}`));
+
+        list.appendChild(li);
+      }
+    });
+  });
